68ad321fd7ca095c423c81046722f0dfd8ed503e
Gifted-Dave <discordbotz526@gmail.com>
2025-08-31 03:46:07 +0300

    dave

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝐃𝐀𝐕𝐄-𝐌𝐃',
+  author: 'SUPREME',
+  botName: "𝐃𝐀𝐕𝐄-𝐌𝐃",
+  botOwner: 'Dave', // Your name
+  ownerNumber: '254798570132', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2.4",
+};
+
+module.exports = settings;
