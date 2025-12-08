30fd1a1e59ecef6b60585b7ce60059be1acc722b
𝘷𝘢𝘳_𝘥𝘢𝘷𝘦 <babydave081@gmail.com>
2025-12-08 06:57:06 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,25 @@
+ const settings = {
+
+  botName: "Dave Md",
+
+  ownerNumber: '',
+
+  packname: 'Dave-Tech',
+
+  prefix: '.',
+
+  defaultReaction: '✅',  
+
+  errorReaction: '❌',
+
+ watermark: "©Dave Tech!",
+
+  botOwner: 'Dave', //Ur-Name
+
+  description: "This is a bot for managing group commands and automating tasks.",
+
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', 
+
+};
+
+export default settings;
